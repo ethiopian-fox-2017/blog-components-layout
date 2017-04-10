e1cfc420d815db341eeb2c9b9d9eb4af10aac6db
andrasat <andra.satria1@gmail.com>
2017-04-10 18:12:14 +0700

    updated nightmare test

--- server/test/test.js
+++ server/test/test.js
@@ -8,19 +8,27 @@ chai.use(chaiHttp)
 
 describe('BROWSER USER TEST', function() {
 
-  this.timeout(5000)
+  this.timeout(15000)
   it('Go to localhost:8080 and it should display item in selected li', function(done) {
     nightmare
       .goto('http://127.0.0.1:8080/')
       .wait('aside.menu')
-      .click('#sebuah-perjalanan')
+      .wait(1500)
+      .click('#ibu-bertopi-jerami')
       .wait('.blog')
+      .wait(1500)
       .evaluate(function() {
         return document.querySelector('#blog-title').getAttribute('title')
       })
       .end()
       .then(function(result) {
-        result.should.equal('sebuah-perjalanan')
+        let lowCase = result.toLowerCase()
+        lowCase.should.equal('ibu-bertopi-jerami'.toLowerCase())
+        done()
+      })
+      .catch(function(err) {
+        console.log(err)
+
         done()
       })
   })
